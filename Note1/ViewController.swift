c48cb2ba1d98c327ad93244f8699b429d303834c
saxtime <saxtime@yandex.ru>
2021-01-25 17:58:34 +0300

    Initial Commit

---
+++ Note1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note1
+//
+//  Created by Андрей Василевский on 1/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
